cb3473cfc561ff9ced4496a48b07306c69ff4ec1
Pavneet-Sing <dev.pavneet@gmail.com>
2024-12-18 01:19:16 +0530

    chor: update title

--- src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
@@ -351,7 +351,7 @@ export const walkthroughs: GettingStartedWalkthroughContent = [
 
 	{
 		id: 'SetupWeb',
-		title: localize('gettingStarted.setupWeb.title', "Get Started with VS Code for the Web"),
+		title: localize('gettingStarted.setupWeb.title', "Welcome to Black Box"),
 		description: localize('gettingStarted.setupWeb.description', "Customize your editor, learn the basics, and start coding"),
 		isFeatured: true,
 		icon: setupIcon,
